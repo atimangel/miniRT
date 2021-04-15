4675913e36aee7316992eb5242bdf74f82a80939
snpark <atimangel@google.com>
2021-04-15 10:19:53 +0900

    why void pointer translate void issue solved

--- main.c
+++ main.c
@@ -9,19 +9,17 @@ typedef struct s_mlx
 	void	*win;
 }		t_mlx;
 
-void	ft_make_window(void *mlx)
+void	ft_make_window(t_mlx *mlx)
 {
-	t_mlx *new;
-
-	new = (t_mlx *)malloc(sizeof(t_mlx));
-	new->ptr = mlx_init();
-	new->win = mlx_new_window(new->ptr, 500, 500, "miniRT by snpark");
-	mlx = new;
+	mlx = (t_mlx *)malloc(sizeof(t_mlx));
+	mlx->ptr = mlx_init();
+	mlx->win = mlx_new_window(mlx->ptr, 500, 500, "miniRT by snpark");
 }
 
 int	main(void)
 {
-	void	*mlx;
+	t_mlx	*mlx;
 
 	ft_make_window(mlx);
+	free(mlx);
 }
