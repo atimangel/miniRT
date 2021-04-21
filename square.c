30c9de0893dfd21bed4a3d1a9b057840557aa807
snagjaepark <atimangel@naver.com>
2021-04-21 21:08:33 +0900

    square corectde

--- square.c
+++ square.c
@@ -15,8 +15,6 @@ void	ft_square_touch(t_ray *r, int *color)
 	t_vec3		*up;
 	t_vec3		*right;
 	t_vec3		*tmp;
-	t_vec3		*v1;
-	t_vec3		*v2;
 	t_vec3		*v3;
 	t_vec3		*v4;
 	t_a16		*m;
@@ -25,9 +23,9 @@ void	ft_square_touch(t_ray *r, int *color)
 	double		gamma;
 	double		t;
 
-	sq.center = ft_vec3(0.0, 0.0, -3.0);
+	sq.center = ft_vec3(0.0, 0.0, -5.0);
 	sq.normal = ft_vec3(0.0, 0.0, -1.0);
-	sq.len	= 0.5;
+	sq.len	= 1.0;
 	abs_up = ft_vec3(0.0, 1.0, 0.0);
 	right = ft_vec3_cross_product(*sq.normal, *abs_up);
 	up = ft_vec3_cross_product(*sq.normal, *right);
@@ -37,12 +35,9 @@ void	ft_square_touch(t_ray *r, int *color)
 	tmp = up;
 	up = ft_vec3_normalize(*up);
 	free(tmp);
-	v1 = ft_vec3_remove(*sq.center, *up);
-	v2 = ft_vec3_remove(*sq.center, *right);
 	v3 = ft_vec3_remove(*sq.center, *r->e);
-	m = ft_matrix(*v1, *v2, *r->d);
+	m = ft_matrix(*up, *right, *r->d);
 	m_inverse = ft_matrix_inverse(*m, ft_matrix_determinant(*m));
-	ft_puta16(*m);
 	v4 = ft_vec3_transform_normal(*m_inverse, *v3);
 	beta = (*v4)[0];
 	gamma = (*v4)[1];
@@ -62,8 +57,6 @@ void	ft_square_touch(t_ray *r, int *color)
 	free(abs_up);
 	free(up);
 	free(right);
-	free(v1);
-	free(v2);
 	free(v3);
 	free(v4);
 	free(m);
