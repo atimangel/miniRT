19d49d95162daf83ba3bd11961cbd3b788f6ee02
snagjaepark <atimangel@naver.com>
2021-04-29 14:56:35 +0900

    distance is matter nowgit add diffuse_reflection.c specular_reflection.c !

--- diffuse_reflection.c
+++ diffuse_reflection.c
@@ -7,23 +7,19 @@ void	ft_diffuse_reflection(t_ray *r, t_pixel_unit *u, t_light light)
 	t_vec3	*lp;
 	double	cos;
 	t_vec3	ratio;
+	double	distance;
 
 	tmp = ft_vec3_scale(*r->d, r->t);
 	point = ft_vec3_add(*r->e, *tmp);
 	free(tmp);
 	lp = ft_vec3_remove(*light.c, *point);
 	cos = ft_vec3_dot_product(*lp, *u->o_n) / ft_vec3_len(*lp) / ft_vec3_len(*u->o_n);
-	printf("cos %flight ratio%f\n", cos, light.ratio);
+	distance = ft_vec3_len(*lp);
 	if (cos > 0)
 	{
-		ratio[0] = light.red * cos * light.ratio / 255;
-		ratio[1] = light.green * cos * light.ratio / 255;
-		ratio[2] = light.blue * cos * light.ratio / 255;
-		u->p_r = u->o_r * ratio[0];
-		u->p_g = u->o_g * ratio[1];
-		u->p_b = u->o_b * ratio[2];
-		printf("ratio %f %f %f\n", ratio[0], ratio[1], ratio[2]);
-		printf("%d %d %d << %d %d %d\n", u->p_r, u->p_g, u->p_b, u->o_r, u->o_g, u->o_b);
+		u->p_r = u->o_r * light.red * cos * light.ratio / 255 / (1 + 0.1 * distance + 0.01 * pow(distance, 2.0));
+		u->p_g = u->o_g * light.green * cos * light.ratio / 255 / (1 + 0.1 * distance + 0.01 * pow(distance, 2.0));
+		u->p_b = u->o_b * light.blue * cos * light.ratio / 255 / (1 + 0.1 * distance + 0.01 * pow(distance, 2.0));
 	}
 	free(point);
 	free(lp);
--- specular_reflection.c
+++ specular_reflection.c
@@ -8,6 +8,7 @@ void	ft_specular_reflection(t_ray *r, t_pixel_unit *u, t_light light)
 	t_vec3	*reflected_ray;
 	t_vec3	*up;
 	double	cos;
+	double	distance;
 
 	tmp = ft_vec3_scale(*r->d, r->t);
 	point = ft_vec3_add(*r->e, *tmp);
@@ -19,11 +20,12 @@ void	ft_specular_reflection(t_ray *r, t_pixel_unit *u, t_light light)
 
 	cos = ft_vec3_dot_product(*reflected_ray, *r->d) / ft_vec3_len(*r->d) / ft_vec3_len(*reflected_ray) * -1;
 	cos = pow(cos, 32);
+	distance = ft_vec3_len(*lp);
 	if (cos > 0)
 	{
-		u->p_r += light.red * cos;
-		u->p_g += light.green * cos;
-		u->p_b += light.blue * cos;
+		u->p_r += light.red * cos / (1 + 0.1 * distance + 0.01 * distance);
+		u->p_g += light.green * cos / (1 + 0.1 * distance + 0.01 * distance);
+		u->p_b += light.blue * cos / (1 + 0.1 * distance + 0.01 * distance);
 	}
 	free(lp);
 	free(point);
