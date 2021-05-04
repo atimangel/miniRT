31ff28039f3b21bdb33294c5d04678a8aaa841ad
snagjaepark <atimangel@naver.com>
2021-05-04 21:39:17 +0900

    make window with obj R and free del list R from struct list

--- make_window.c
+++ make_window.c
@@ -7,13 +7,12 @@ void	ft_make_window(t_mlx *mlx, t_list *obj)
 	while (((t_resolution *)obj->content)->id != R)
 		obj = obj->next;
 	if (!obj)
-	{
 		printf("error\n where is resolution value?");
-	}
 	else
 	{
 		res = obj->content;
 		mlx->ptr = mlx_init();
 		mlx->win = mlx_new_window(mlx->ptr, res->x, res->y, "miniRT by snpark");
+		ft_lstdelone(obj, free);
 	}
 }
