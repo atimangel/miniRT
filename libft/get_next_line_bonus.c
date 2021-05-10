18633e543e5b30c437276934b56c66c9f42b45f5
snpark <snpark@student.42seoul.kr>
2021-05-10 14:57:10 +0900

    if re read ft_str if(*ft_strchr(src, '\n') == '\n') is problem ft_strchr return not int but pointer if not found \n return NULL == 0 so it will be *(NULL) we can't acces to NULL pointer but it try to so segfault keep happen

--- libft/get_next_line_bonus.c
+++ libft/get_next_line_bonus.c
@@ -20,17 +20,26 @@ static int	sub_get(char **dest, char *src, char **save)
 	tmp = *dest;
 	*dest = ft_strjoin(*dest, src);
 	free(tmp);
-	if (*ft_strchr(src, '\n') == '\n')
+	if (!*dest)
+		return (-1);
+	if (ft_strchr(src, '\n') != 0)
 	{
 		tmp = *dest;
 		*dest = ft_substr(*dest, 0, ft_strchr(*dest, '\n') - *dest);
 		free(tmp);
+		if (!*dest)
+			return (-1);
 		if (*(ft_strchr(src, '\n') + 1) != '\0')
+		{
 			*save = ft_substr(src, ft_strchr(src, '\n') - src + 1,
 					ft_strlen(src) - (ft_strchr(src, '\n') - src)- 1);
+			if (!*save)
+				return (-1);
+		}
 		return (1);
 	}
 	return (0);
+
 }
 
 static void	ft_free_save(char *tmp, char **save)
@@ -56,15 +65,15 @@ int			get_next_line(int fd, char **line)
 		tmp = save;
 		return_subget = sub_get(line, save, &save);
 		ft_free_save(tmp, &save);
-		if (return_subget == 1)
-			return (1);
+		if (return_subget != 0)
+			return (return_subget);
 	}
 	while ((readnum = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
 		buf[readnum] = '\0';
 		return_subget = sub_get(line, buf, &save);
-		if (return_subget == 1)
-			return (1);
+		if (return_subget != 0)
+			return (return_subget);
 	}
 	return (readnum > 0 ? 1 : readnum);
 }
