0b4d0ec646eb32e19bc2141283ab64dd13297239
snagjaepark <atimangel@naver.com>
2021-05-11 14:51:53 +0900

    trace.t is reset

--- reflection.c
+++ reflection.c
@@ -9,18 +9,18 @@ void	ft_reflection(t_ray r, t_pixel_unit *u, t_list *obj)
 	tmp = ft_vec3_scale(*r.d, r.t);
 	trace.d = ft_vec3_add(r.e, *tmp);
 	free(tmp);
-	trace.t = 1.0;
 	while(obj)
 	{
 		if (obj->id == l)
 		{
+			trace.t = 1.0;
 			li = obj->content;
 			ft_memcpy(&trace.e, &li->point, sizeof(t_vec3));
 			ft_touch(&trace, u, obj, 1);
 			if (trace.t == 1.0)
 			{
 				ft_diffuse_reflection(trace, u, *li);
-				ft_specular_reflection(r, trace, u, *li);
+				//ft_specular_reflection(r, trace, u, *li);
 			}
 		}
 		obj = obj->next;
